6a3684d6acac01d633b635e8b1bc3ad3fb4841e1
akima <akm2000@gmail.com>
2013-10-07 20:10:10 +0900

    letとsubjectをbefore:allから呼び出すとwarningがでるので、修正しました

--- spec/scm_workspace_spec.rb
+++ spec/scm_workspace_spec.rb
@@ -7,27 +7,27 @@ require 'fileutils'
 # ApiServerモードでもScmWorkspaceクラスを使用するので、参照だけはできることを確認する。
 describe ScmWorkspace do
 
-  before(:all){ @tmp_workspace = Dir.mktmpdir }
+  before(:all) do
+    @tmp_workspace = Dir.mktmpdir
+    @scm_workspace = ScmWorkspace.new(workspace: @tmp_workspace)
+  end
   after(:all){ FileUtils.remove_entry_secure(@tmp_workspace) }
-  let(:tmp_workspace){ @tmp_workspace }
-  let(:scm_workspace){ ScmWorkspace.new(workspace: tmp_workspace) }
 
   context "not depend on git" do
-    subject{ scm_workspace }
+    subject{ @scm_workspace }
     its(:repo_dir){ should_not == nil }
-    its(:options_path){ should_not == nil }
     it{ subject.configured? }
     it{ subject.cleared? }
   end
 
   context "git", git: true do
-    let(:url){ "git://github.com/akm/git_sandbox.git" }
+    before(:all){ @url = "git://github.com/akm/git_sandbox.git" }
 
     context "before configuring" do
       before do
-        FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
+        FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
       end
-      subject{ scm_workspace }
+      subject{ @scm_workspace }
 
       its(:configured?){ should == false}
       its(:scm_type){ should == nil}
@@ -48,15 +48,14 @@ describe ScmWorkspace do
 
     context "after configuring" do
       before(:all) do
-        FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-        scm_workspace.configure(url)
+        FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+        @scm_workspace.configure(@url)
       end
-      subject{ scm_workspace }
+      subject{ @scm_workspace }
 
-      its(:options){ should == {"url" => url} }
       its(:configured?){ should == true}
       its(:cleared?){ should == false }
-      its(:url){ should == url }
+      its(:url){ should == @url }
 
       its(:scm_type){ should == :git}
       its(:git_repo?){ should == true }
@@ -71,13 +70,13 @@ describe ScmWorkspace do
       its(:current_commit_key){ should == "a0eaf8cca31080ca26edbae0daddaa9931edd6d6" }
 
       it :fetch do
-        scm_workspace.fetch
+        @scm_workspace.fetch
       end
 
       context "duplicated configuration" do
         it do
           expect{
-            scm_workspace.configure(url)
+            @scm_workspace.configure(@url)
           }.to raise_error(/not empty/i)
         end
       end
@@ -86,28 +85,28 @@ describe ScmWorkspace do
     describe :checkout do
       context "another branch" do
         before(:all) do
-          FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-          scm_workspace.configure(url)
-          scm_workspace.checkout("0.1")
+          FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+          @scm_workspace.configure(@url)
+          @scm_workspace.checkout("0.1")
         end
-        subject{ scm_workspace }
+        subject{ @scm_workspace }
         its(:current_branch_name){ should == "0.1" }
         its(:current_tag_names){ should =~ %w[v0.1.0] }
       end
 
       context "branch is updated" do
         before(:all) do
-          FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-          scm_workspace.configure(url)
-          Dir.chdir(scm_workspace.repo_dir) do
+          FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+          @scm_workspace.configure(@url)
+          Dir.chdir(@scm_workspace.repo_dir) do
             # バージョンを巻き戻す
             system("git reset --hard a68a53610407e6ba30939326af743d3b734bb53d")
           end
         end
         it "checkout again" do
-          scm_workspace.status.should =~ /7 commits/
-          scm_workspace.checkout("develop")
-          scm_workspace.status.should =~ /up-to-dated/
+          @scm_workspace.status.should =~ /7 commits/
+          @scm_workspace.checkout("develop")
+          @scm_workspace.status.should =~ /up-to-dated/
         end
       end
     end
@@ -116,24 +115,24 @@ describe ScmWorkspace do
       %w[v0.0.1 v0.0.2 v0.1.0].each do |tag|
         context "#{tag} in develop" do
           before(:all) do
-            FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-            scm_workspace.configure(url)
-            scm_workspace.checkout("develop")
-            scm_workspace.move(tag)
+            FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+            @scm_workspace.configure(@url)
+            @scm_workspace.checkout("develop")
+            @scm_workspace.move(tag)
           end
-          subject{ scm_workspace }
+          subject{ @scm_workspace }
           its(:current_branch_name){ should == "develop" }
           its(:current_tag_names){ should =~ [tag] }
         end
       end
       context "reset to fix wrong reset" do
         before(:all) do
-          FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-          scm_workspace.configure(url)
-          scm_workspace.checkout("develop")
-          scm_workspace.move("v0.1.0")
+          FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+          @scm_workspace.configure(@url)
+          @scm_workspace.checkout("develop")
+          @scm_workspace.move("v0.1.0")
         end
-        subject{ scm_workspace }
+        subject{ @scm_workspace }
         it do
           subject.current_branch_name.should == "develop"
           subject.current_tag_names.should =~ ["v0.1.0"]
@@ -146,35 +145,36 @@ describe ScmWorkspace do
 
     describe :clear do
       before(:all) do
-        FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-        scm_workspace.configure(url)
+        FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+        @scm_workspace.configure(@url)
       end
 
       it do
-        scm_workspace.cleared?.should == false
-        scm_workspace.clear
-        scm_workspace.cleared?.should == true
-        Dir.exist?(scm_workspace.repo_dir).should == false
+        @scm_workspace.cleared?.should == false
+        @scm_workspace.clear
+        @scm_workspace.cleared?.should == true
+        Dir.exist?(@scm_workspace.repo_dir).should == false
       end
     end
 
   end
 
   context "svn", svn: true do
-    let(:base_url){ "http://rubeus.googlecode.com/svn" }
-    let(:cloning_url){ "#{base_url} -T trunk --branches branches --tags tags" }
+    before(:all) do
+      @base_url = "http://rubeus.googlecode.com/svn"
+      @cloning_url = "#{@base_url} -T trunk --branches branches --tags tags"
+    end
 
     context "after configuring" do
       before(:all) do
-        FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-        scm_workspace.configure(cloning_url)
+        FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+        @scm_workspace.configure(@cloning_url)
       end
-      subject{ scm_workspace }
+      subject{ @scm_workspace }
 
       its(:configured?){ should == true}
       its(:cleared?){ should == false }
-      its(:url){ should == base_url }
-      its(:options){ should == {"url" => base_url, "trunk" => "trunk", "branches" => "branches", "tags" => "tags"} }
+      its(:url){ should == @base_url }
 
       its(:scm_type){ should == :svn}
       its(:git_repo?){ should == false }
@@ -190,20 +190,20 @@ describe ScmWorkspace do
       its(:current_commit_key){ should =~ /\A[0-9a-f]{40}:247\Z/ }
 
       it :fetch do
-        scm_workspace.fetch
+        @scm_workspace.fetch
       end
     end
 
     context "duplicated configuration" do
       before(:all) do
-        FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-        scm_workspace.configure(cloning_url)
+        FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+        @scm_workspace.configure(@cloning_url)
       end
-      subject{ scm_workspace }
+      subject{ @scm_workspace }
 
       it do
         expect{
-          scm_workspace.configure(cloning_url)
+          @scm_workspace.configure(@cloning_url)
         }.to raise_error(/not empty/i)
       end
     end
@@ -213,11 +213,11 @@ describe ScmWorkspace do
 
         context branch_name do
           before(:all) do
-            FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-            scm_workspace.configure(cloning_url)
-            scm_workspace.checkout(branch_name)
+            FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+            @scm_workspace.configure(@cloning_url)
+            @scm_workspace.checkout(branch_name)
           end
-          subject{ scm_workspace }
+          subject{ @scm_workspace }
           its(:current_branch_name){ should == branch_name }
           its(:current_tag_names){ should == [] }
         end
@@ -226,21 +226,21 @@ describe ScmWorkspace do
 
       context "branch is updated" do
         before(:all) do
-          FileUtils.remove_entry_secure(scm_workspace.repo_dir) if Dir.exist?(scm_workspace.repo_dir)
-          scm_workspace.configure(cloning_url)
-          scm_workspace.checkout("trunk")
-          Dir.chdir(scm_workspace.repo_dir) do
+          FileUtils.remove_entry_secure(@scm_workspace.repo_dir) if Dir.exist?(@scm_workspace.repo_dir)
+          @scm_workspace.configure(@cloning_url)
+          @scm_workspace.checkout("trunk")
+          Dir.chdir(@scm_workspace.repo_dir) do
             # バージョンを巻き戻す
             system("git reset --hard HEAD~3")
           end
         end
 
         it "checkout again" do
-          scm_workspace.current_commit_key.should =~ /\A[0-9a-f]{40}:244\Z/
-          scm_workspace.status.should =~ /3 commits.*current revision: 244.*latest revision: 247/
-          scm_workspace.checkout("trunk")
-          scm_workspace.current_commit_key.should =~ /\A[0-9a-f]{40}:247\Z/
-          scm_workspace.status.should =~ /up-to-dated/
+          @scm_workspace.current_commit_key.should =~ /\A[0-9a-f]{40}:244\Z/
+          @scm_workspace.status.should =~ /3 commits.*current revision: 244.*latest revision: 247/
+          @scm_workspace.checkout("trunk")
+          @scm_workspace.current_commit_key.should =~ /\A[0-9a-f]{40}:247\Z/
+          @scm_workspace.status.should =~ /up-to-dated/
         end
 
       end
